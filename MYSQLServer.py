a65ade7cf9b00d71abec0cf2ef4287aec76f641a
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-07-02 21:41:58 +0300

    Add MySQL connection script to create 'alx_book_store' database

---
+++ MYSQLServer.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+# Load environment variables from .env file
+load_dotenv()
+
+try:
+
+    # Connect to the MySQL server
+    connection = mysql.connector.connect(
+        host = os.getenv('DB_HOST'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        # Create a new database
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully.")
+
+except mysql.connector.Error as err:
+    print(f"Error while connecting to MySQL: {err}")
+
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed.")
+    else:
+        print("No MySQL connection to close.")
